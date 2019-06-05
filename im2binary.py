c62a59610ab2621430492a43280f0b7ddc703086
Rehan Ahmed <rhnahdshk@gmail.com>
2019-06-05 09:20:38 +0500

    first commit

---
+++ im2binary.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 08:35:27 2019
+
+@author: rehan
+"""
+
+from PIL import Image
+import os 
+import numpy
+import sys
+
+SIZE = 200, 200
+IMAGEPATH = '123.jpg'
+
+img = Image.open(IMAGEPATH).convert('LA')
+img.thumbnail(SIZE, Image.ANTIALIAS)
+np_im = numpy.array(img)
+
+
+txtOut = open('output.txt','w+')
+
+ans=""
+for j in range(0,len(np_im)):
+    for i in range(0,len(np_im[0])):
+        
+        if list(np_im[j,i])[0]>127.5:
+            ans=ans+str(0)
+        else:
+            ans=ans+str(1)
+    ans=ans+'\n'
+
+txtOut.write(ans)
+txtOut.close()
